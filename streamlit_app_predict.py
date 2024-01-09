b3d2e3f8eab454332d643f5a7db7843de7e407c3
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2024-01-09 11:49:42 +0530

    ...

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -97,8 +97,8 @@ def drawBoundingBoxDefect(saved_image ,x, y, w, h, cl, cf):
     txt_start_pnt = (x-w//2, y-h//2-15)
     
     
-    img = cv2.rectangle(img, start_pnt, end_pnt, (0,255,0), 10)
-    img = cv2.putText(img, cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 10, cv2.LINE_AA)		
+    img = cv2.rectangle(img, start_pnt, end_pnt, (0,0,255), 10)
+    img = cv2.putText(img, cl, txt_start_pnt, cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 10, cv2.LINE_AA)		
     
     return img
 
